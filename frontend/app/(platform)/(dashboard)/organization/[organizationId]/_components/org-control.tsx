6b7b299ec925b450d582a14b87d8577588cc1fac
mattellis91 <mathew.ellis2@griffithuni.edu.au>
2024-09-06 13:30:29 +1000

    removed nextjs frontend with react

--- frontend/app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { useEffect } from "react"
-import { useParams } from "next/navigation"
-
-export const OrgControl = () => {
-    const params = useParams();
-    return null;
-}
--- frontend/config/site.ts
+++
@@ -1,5 +0,0 @@
-
-export const siteConfig = {
-    name: "Taskify",
-    description: "Collaborate, manage projects, and reach new productivity peaks"
-};
